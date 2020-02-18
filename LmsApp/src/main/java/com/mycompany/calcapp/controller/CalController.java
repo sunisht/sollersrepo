7872eb58d7dfd94e61b4ffcb38392d97c7704229
lnkafu <Atabong1212>
2020-02-18 16:40:50 -0500

    LN - Initial commit

---
+++ LmsApp/src/main/java/com/mycompany/calcapp/controller/CalController.java
@@ -0,0 +1,38 @@
+package com.mycompany.calcapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mycompany.calcapp.domain.MathOperation;
+import com.mycompany.calcapp.services.CalculatorServices;
+
+@Controller
+public class CalController {
+	@Autowired
+	CalculatorServices services;
+	
+	@ResponseBody
+	@RequestMapping(value = "/AllOperationsPerformed", method = RequestMethod.GET)
+	public List<MathOperation> readAll() {
+		return null;
+	}
+	@RequestMapping(value = "/Addition", method = RequestMethod.POST)
+	@ResponseBody
+	public MathOperation add(@RequestBody MathOperation mathObject) {
+		return null;
+	}
+	
+	@RequestMapping(value = "/Substraction", method = RequestMethod.POST)
+	@ResponseBody
+	public MathOperation Substract(@RequestBody MathOperation mathObject) {
+		return null;
+	}
+
+
+}
---
+++ LmsApp/src/main/java/com/mycompany/calcapp/domain/MathOperation.java
@@ -0,0 +1,60 @@
+package com.mycompany.calcapp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MathOperations")
+public class MathOperation {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	Integer id;
+	@Column
+	private double number1;
+	@Column
+	private double number2;
+	@Column
+	private double result;
+	
+	@Column(name="Operation Performed")
+	private String operation;
+	
+	public double getNumber1() {
+		return number1;
+	}
+	public void setNumber1(double number1) {
+		this.number1 = number1;
+	}
+	public double getNumber2() {
+		return number2;
+	}
+	public void setNumber2(double number2) {
+		this.number2 = number2;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public double getResult() {
+		return result;
+	}
+	public void setResult(double result) {
+		this.result = result;
+	}
+	public String getOperation() {
+		return operation;
+	}
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	
+	
+
+}
