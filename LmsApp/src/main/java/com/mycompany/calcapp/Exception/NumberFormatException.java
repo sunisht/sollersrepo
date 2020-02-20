ab12b9662e18d45a52b99c59243854a7b6e0c2b7
lnkafu <Atabong1212>
2020-02-20 14:27:19 -0500

    exceptions added

---
+++ LmsApp/src/main/java/com/mycompany/calcapp/Exception/NumberFormatException.java
@@ -0,0 +1,14 @@
+package com.mycompany.calcapp.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND,reason="Number Format Exception")
+public class NumberFormatException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
